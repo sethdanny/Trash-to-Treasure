075ce3325b07d29c071b2221384b1b4cf9ec4c34
sethdanny <naddulidaniel1994@gmail.com>
2023-05-30 11:46:52 +0300

    handling backend logic

--- backend/server.js
+++ backend/server.js
@@ -113,6 +113,62 @@ app.delete('/api/waste/:id', (req, res) => {
   res.json({ message: 'Delete waste listing', data: { id: wasteListingId } });
 });
 
+
+// POST request to create a pickup request
+app.post('/pickup-requests', (req, res) => {
+  // Logic to create a new pickup request
+  // Access the request body to get the pickup request details
+  const pickupRequestDetails = req.body;
+  
+  // Process the pickup request details and create a new pickup request in the database
+  // Return the created pickup request object as a response
+  res.json({ pickupRequest: 'created-pickup-request' });
+});
+
+// GET request to retrieve pickup requests
+app.get('/pickup-requests', (req, res) => {
+  // Logic to retrieve pickup requests based on filters or criteria
+  // Access query parameters to determine the filters (e.g., location, waste type, availability)
+  
+  // Query the database and fetch pickup requests that match the specified filters
+  // Return the retrieved pickup requests as a response
+  res.json({ pickupRequests: 'retrieved-pickup-requests' });
+});
+
+// PUT request to update a pickup request
+app.put('/pickup-requests/:pickupRequestId', (req, res) => {
+  // Logic to update a pickup request
+  // Access the pickup request ID from the URL parameters
+  const pickupRequestId = req.params.pickupRequestId;
+
+  // Access the request body to get the updated pickup request details
+  const updatedPickupRequestDetails = req.body;
+
+  // Find the pickup request in the database by its ID and update its details
+  // Return the updated pickup request object as a response
+  res.json({ pickupRequest: 'updated-pickup-request' });
+});
+
+// DELETE request to cancel a pickup request
+app.delete('/pickup-requests/:pickupRequestId', (req, res) => {
+  // Logic to cancel a pickup request
+  // Access the pickup request ID from the URL parameters
+  const pickupRequestId = req.params.pickupRequestId;
+
+  // Find the pickup request in the database by its ID and mark it as canceled
+  // Return a success message as a response
+  res.json({ message: 'Pickup request canceled successfully' });
+});
+
+// Route for initiating payment
+app.post('/api/payment/initiate', paymentController.initiatePayment);
+
+// Route for retrieving payment details
+app.get('/api/payment/:paymentId', paymentController.getPaymentDetails);
+
+// Route for handling payment callbacks
+app.post('/api/payment/callback', paymentController.handlePaymentCallback);
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
