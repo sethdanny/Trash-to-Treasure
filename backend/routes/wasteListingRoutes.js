0dab00ccde2103eb486f235d813105364acbeded
sethdanny <naddulidaniel1994@gmail.com>
2023-05-31 18:37:12 +0300

    fix

--- backend/routes/wasteListingRoutes.js
+++ backend/routes/wasteListingRoutes.js
@@ -3,46 +3,17 @@
 
 const express = require('express');
 const router = express.Router();
+const {
+  getWasteListing,
+  createWasteListing,
+  updateWasteListing,
+  deleteWasteListing
+} = require('../controllers/wasteListingController');
 
-// GET request to fetch waste listings
-router.get('/', async (req, res) => {
-  // Logic to fetch waste listings from the database
-  // Return the fetched waste listings as a response
-  res.status(200).json({ message: 'Fetch waste listings' });
-});
-
-// POST request to create a new waste listing
-router.post('/', async (req, res) => {
-  // Logic to create a new waste listing in the database
-  // Access the request body to get the details of the waste listing
-  const wasteListing = console.log(req.body);
-
-  // Process the waste listing and store it in the database
-  // Return the created waste listing as a response
-  res.status(200).json({ message: 'Create new waste listing', data: wasteListing });
-});
-
-// PUT request to update a waste listing
-router.put('/:id', async (req, res) => {
-  // Logic to update a waste listing in the database
-  // Access the request parameters (e.g., ID) and body to get the updated details
-  const wasteListingId = req.params.id;
-  const updatedWasteListing = req.body;
-
-  // Update the waste listing in the database based on the provided ID
-  // Return the updated waste listing as a response
-  res.status(200).json({ message: 'Update waste listing', data: { id: wasteListingId, ...updatedWasteListing } });
-});
-
-// DELETE request to delete a waste listing
-router.delete('/:id', async (req, res) => {
-  // Logic to delete a waste listing from the database
-  // Access the request parameter (e.g., ID) to determine which waste listing to delete
-  const wasteListingId = req.params.id;
-
-  // Delete the waste listing from the database based on the provided ID
-  // Return a success message as a response
-  res.status(200).json({ message: 'Delete waste listing', data: { id: wasteListingId } });
-});
+// Routes to handle waste listing
+router.get('/', getWasteListing);
+router.post('/', createWasteListing);
+router.put('/:id', updateWasteListing);
+router.delete('/:id', deleteWasteListing);
 
 module.exports = router;
