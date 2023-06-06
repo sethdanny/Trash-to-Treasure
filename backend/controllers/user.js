0c5a6cc0cd40b731e085e1db5513c2b1e67417aa
sethdanny <naddulidaniel1994@gmail.com>
2023-06-07 00:16:25 +0300

    fix

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -99,24 +99,13 @@ const generateToken = async (user, statusCode, res) => {
   }
 };
 
-const userLogout = async (req, res) => {
+const userLogout = async (req, res, next) => {
   try {
-    const { userId, token } = req;
-
-    // Find the user by ID
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    // Remove the token from the invalidatedTokens array
-    user.invalidatedTokens = user.invalidatedTokens.filter((t) => t !== token);
-
-    // Save the updated user
-    await user.save();
-
-    res.json({ message: 'Logout successful' });
+    res.clearCookie('token');
+    res.status(200).json({
+      success: true,
+      message: 'logged out successfully'
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
