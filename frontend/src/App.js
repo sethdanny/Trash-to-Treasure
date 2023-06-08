c64866e624519602bf04216378ef21532d59efe0
sethdanny <naddulidaniel1994@gmail.com>
2023-06-08 16:43:59 +0300

    created frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>welcome to react</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
